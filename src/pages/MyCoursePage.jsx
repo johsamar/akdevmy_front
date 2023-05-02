1d01d06d8f7c9bb8a70e2c1804dbcaabce91379b
Juan Restrepo <juanmanuelrestrepo1995@gmail.com>
2023-05-02 13:58:30 -0500

    feat: add search bar

--- src/pages/MyCoursePage.jsx
+++ src/pages/MyCoursePage.jsx
@@ -5,29 +5,57 @@ import "../styles/MyCoursePage.css";
 
 const MyCoursePage = () => {
   const [courses, setCourses] = useState([]);
+  const [filteredCourses, setFilteredCourses] = useState([]);
 
-  const getCharacterFromService = async () => {
+  const getCoursesFromService = async () => {
     let coursesList = await getCourses();
     setCourses(coursesList.data);
+    setFilteredCourses(coursesList.data);
     console.log(coursesList.data); //! to delete
   };
 
   useEffect(() => {
-    getCharacterFromService();
+    getCoursesFromService();
   }, []);
+
+  const handleChange = (e) => {
+    let textToFilter = e.target.value;
+    //console.log(e.target.value);
+
+    const filter = courses.filter((course) =>
+      course.name.toLowerCase().includes(textToFilter.toLowerCase())
+    );
+
+    setFilteredCourses(filter);
+  };
 
   return (
     <>
-      <div className="container-fluid">
+      <div className="containerPage">
         {/* My Courses title */}
+        <p className="title-section-course">Mis cursos</p>
 
         {/* Search filter */}
+        <div className="searchBarContainer">
+          <div className="containerInput">
+            <input
+              className="inputBuscar form-control"
+              placeholder="Busqueda por nombre del curso"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
 
         {/* Course cards */}
         <div className="rows-container">
-          {courses &&
-            courses.map((course) => {
-              return <GeneralCardComponent key={course.courseId} singleElement={course} />;
+          {filteredCourses &&
+            filteredCourses.map((course) => {
+              return (
+                <GeneralCardComponent
+                  key={course.courseId}
+                  singleElement={course}
+                />
+              );
             })}
         </div>
 
