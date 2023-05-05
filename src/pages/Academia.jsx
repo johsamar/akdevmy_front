fa1730fe0af4391a3afe5c6955223b3c647745c2
alejoa27 <alejandro.agudelo2@utp.edu.co>
2023-05-04 21:40:21 -0500

    feat(information):Add endpoint view academy information and create multimedia component ready for use

--- src/pages/Academia.jsx
+++ src/pages/Academia.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { findByID } from "../services/AcademyService";
+
 
 const Academia = () => {
+    const [posts, setPosts] = useState([]);
+    const getAcademyService = async () => {
+        const data = await findByID("64503fa1b65d80d35a1831eb");
+        setPosts(data.data);
+    };
+    useEffect(() => {
+        getAcademyService();
+    }, []);
     return (
         <>
             <div className="container vh-100">
@@ -23,15 +33,14 @@ const Academia = () => {
                                 <p>Redes sociales: </p>
                             </div>
                             <div className="col-4">
-                                <p>La Academia</p>
-                                <p>100000100</p>
-                                <p>88888</p>
-                                <p>lacademia@academy.com</p>
+                                <p>{posts?.name}</p>
+                                <p>{posts?.nit}</p>
+                                <p>{posts?.telephone}</p>
+                                <p>{posts?.email}</p>
                                 <p>Lorem ipsum dolor </p>
                                 <p>26</p>
                                 <p>101</p>
                                 <p>
-
                                     <div className="btn-group">
                                         <button type="button" className="btn btn-outline-secondary">
                                             <i className="bi bi-facebook"></i>
@@ -57,7 +66,7 @@ const Academia = () => {
                         </div>
                         <div className="row d-flex justify-content-end">
                             <div className="col-2 d-flex justify-content-end">
-                            <button type="button" className="btn btn-primary button-edit m-0">Editar</button>
+                                <button type="button" className="btn btn-primary button-edit m-0">Editar</button>
                             </div>
                         </div>
                     </div>
